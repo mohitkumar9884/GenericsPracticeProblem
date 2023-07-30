9427b03c8be03bad04d2ade1d4f3a7290f5b6882
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-30 19:40:42 +0530

    [mohitkumar9884] adding/modifying program FindMaxValueUC1

---
+++ GenericsPracticeProblem/FindMaxValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProblem
+{
+    internal class FindMaxValue
+    {
+        public static void FindMax(int first,int second,int third)
+        {
+            int result = first.CompareTo(second);//0 or 1 or -1
+
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is greater ", first);
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is greater", second);
+            else
+                Console.WriteLine("{0} ise greater", third);
+        }
+    }
+}
--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
+            int[] intArray = { 58, 67, 98, 45 };
+            double[] doubleArray = { 20.25, 40.31, 68.99, 43.32 };
+            char[] charArray = { 'D', 'R', 'E', 'A', 'M' };
+            int x = 25, y = 98, z = 19;
+
             Console.WriteLine("Welcome to GenericsPracticeProblem");
+            Console.WriteLine("Please choose any one program from below options.");
+            Console.WriteLine("\n1.FindMaxValue ");
+            Console.WriteLine(" ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Given input are {0},{1},{2}",x,y,z);
+                    FindMaxValue.FindMax(x, y, z);
+                    break;
+                   
+
+                default:
+                    Console.WriteLine("Please choose number within given options.");
+                    break;
+            }
         }
     }
 }
