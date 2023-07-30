400da0cfff4892954fb15e8c7ca2f29832a63c0d
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-30 19:58:48 +0530

    [mohitkumar9884] adding/modifying program in  FloatFindMaxValueUC2

--- GenericsPracticeProblem/FindMaxValue.cs
+++ GenericsPracticeProblem/FindMaxValue.cs
@@ -20,5 +20,16 @@ namespace GenericsPracticeProblem
             else
                 Console.WriteLine("{0} ise greater", third);
         }
+        public static void FindMax(float first, float second, float third)
+        {
+            //int result = first.CompareTo(second);//0 or 1 or -1
+
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is greater ", first);
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is greater", second);
+            else
+                Console.WriteLine("{0} ise greater", third);
+        }
     }
 }
--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -8,10 +8,11 @@
             double[] doubleArray = { 20.25, 40.31, 68.99, 43.32 };
             char[] charArray = { 'D', 'R', 'E', 'A', 'M' };
             int x = 25, y = 98, z = 19;
+            float a = 25.98f, b = 98.19f, c = 19.25f;
 
             Console.WriteLine("Welcome to GenericsPracticeProblem");
             Console.WriteLine("Please choose any one program from below options.");
-            Console.WriteLine("\n1.FindMaxValue ");
+            Console.WriteLine("\n.1.IntegerFindMaxValue\n.2.FloatFindMaxValue\n.3. ");
             Console.WriteLine(" ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -19,6 +20,10 @@
                 case 1:
                     Console.WriteLine("Given input are {0},{1},{2}",x,y,z);
                     FindMaxValue.FindMax(x, y, z);
+                    break;
+                case 2:
+                    Console.WriteLine("Given input are {0},{1},{2}",a,b,c);
+                    FindMaxValue.FindMax(a, b, c);
                     break;
                    
 
